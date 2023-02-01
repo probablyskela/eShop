4578928d15d1f34108c0388e47411080bd5b2fc0
Oleksandr Yovbak <oleksandr.yovbak@gmail.com>
2023-02-01 12:50:19 +0200

    Homework done

---
+++ Catalog.Host/Merchandise.cs
@@ -0,0 +1,8 @@
+namespace Catalog.Host;
+
+public class Merchandise
+{
+    public string Name { get; set; } = null!;
+    public double Price { get; set; }
+    public int Amount { get; set; }
+}
--- Catalog.Host/Program.cs
+++ Catalog.Host/Program.cs
@@ -16,12 +16,10 @@ if (app.Environment.IsDevelopment())
     app.UseSwaggerUI();
 }
 
-app.UseHttpsRedirection();
+// app.UseHttpsRedirection();
 
 app.UseAuthorization();
 
 app.MapControllers();
 
 app.Run();
-
-// Do homework
